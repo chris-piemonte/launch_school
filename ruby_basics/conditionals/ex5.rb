3c8fe82689d7fa298c625c98409992f45cc6765d
Chris Piemonte <chrisjpiemonte@gmail.com>
2022-10-15 19:40:42 -0500

    Adding a directory for the completed conditionals exercises.

---
+++ ruby_basics/conditionals/ex5.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Why?
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+# It will put "My favorite number is 7" because variables are true by default.
