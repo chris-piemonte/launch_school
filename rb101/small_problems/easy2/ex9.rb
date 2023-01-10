f62732f3fde25be1c8b78656d0d542f10f43a9d7
Chris Piemonte <chrisjpiemonte@gmail.com>
2023-01-09 20:32:58 -0600

    Adding small change to rb101-rb109 exercises easy 2 ex9.

--- rb101/small_problems/easy2/ex9.rb
+++ rb101/small_problems/easy2/ex9.rb
@@ -5,4 +5,3 @@ Then name is assigned to a new name/address in memory so they are pointing to di
 In the second example after both variables are pointing to the same address in memory, the naem variable mutates the caller which mutates the address in memory so both 
 variables are still pointing to the same address/name.
 =end
-
