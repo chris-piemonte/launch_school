5ccadcdcb113057f4701a97deb8163ecb9b46531
Chris Piemonte <chrisjpiemonte@gmail.com>
2023-01-09 20:31:08 -0600

    Adding the lesson 3 directory and the completed Easy 1 Practice Problem set.

---
+++ rb101/lesson_3/easy1/question2.rb
@@ -0,0 +1,18 @@
+=begin
+
+Describe the difference between ! and ? in Ruby. And explain what would happen in the following scenarios:
+
+1 what is != and where should you use it?
+  not equal, when saying you want something to happen when a given variable or scenario does not equal something
+2 put ! before something, like !user_name
+  turns an object into the opposite of it's boolean equivalent
+3 put ! after something, like words.uniq!
+    mutates the caller
+4 put ? before something
+  ternary operator
+5 put ? after something
+  asks if something is something, such as if a number is even
+6 put !! before something, like !!user_name
+  turns an object into their boolean equivalent
+
+  =end
---
+++ rb101/lesson_3/easy1/question4.rb
@@ -0,0 +1,4 @@
+numbers = [1, 2, 3, 4, 5]
+
+puts numbers.delete_at(1) # deletes & returns the element at index 1 => 2
+puts numbers.delete(1) # deletes & returns all elements that match the argument => 1
