be63a677090e4b3e3c00aadd75520362628c6c09
Chris Piemonte <chrisjpiemonte@gmail.com>
2023-01-12 19:42:57 -0600

    Adding a directory for and the completed practice problems of Lesson 3 Hard 1

---
+++ rb101/lesson_3/hard1/ex4.rb
@@ -0,0 +1,11 @@
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words == 4
+  
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false if is_an_ip_number(word)
+  end
+  
+  return true
+end
