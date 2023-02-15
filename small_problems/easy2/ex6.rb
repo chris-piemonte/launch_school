8196a4ff35010e436904be88d9d5dcfcf82d5785
Chris Piemonte <chrisjpiemonte@gmail.com>
2023-02-14 19:37:26 -0600

    Changing the location of the 'small_problems' directory.

---
+++ small_problems/easy2/ex6.rb
@@ -0,0 +1,5 @@
+# (1..99).to_a.each { |i| puts i if i.odd? }
+
+# puts (1..10).to_a.select { |i| i.odd? }
+
+1.upto(99) { |i| puts i if i.odd? }
---
+++ small_problems/easy2/ex7.rb
@@ -0,0 +1,1 @@
+(1..99).to_a.each { |i| puts i if i.even? }
