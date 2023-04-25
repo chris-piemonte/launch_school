22281b959d155764d2a7618ee321b1bf5c423ef9
Chris Piemonte <chrisjpiemonte@gmail.com>
2023-04-24 20:30:19 -0500

    quit code adjustment on ex4

--- small_problems/easy8/ex4.rb
+++ small_problems/easy8/ex4.rb
@@ -39,13 +39,13 @@ def palindromes(string)
   end
 end
 
-p palindromes('abcd') #== []
-p palindromes('madam') #== ['madam', 'ada']
-p palindromes('hello-madam-did-madam-goodbye') #== [
-  # 'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
-  # 'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
-  # '-madam-', 'madam', 'ada', 'oo'
-# ]
-palindromes('knitting cassettes') #== [
-#   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
-# ]
+p palindromes('abcd') == []
+p palindromes('madam') == ['madam', 'ada']
+p palindromes('hello-madam-did-madam-goodbye') == [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+]
+palindromes('knitting cassettes') == [
+  'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+]
